911edb8803aad3616397564364158025a02dd013
Karol7010 <kciagwa@proton.me>
2023-07-02 16:41:27 +0200

    Reader thread completed.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 /*
-The CPU usage tracker exclusively operates within the Linux environment.
+The CPU usage tracker exclusively operates within 
+the Linux environment.
 */
 
 #include "stdio.h"
@@ -7,23 +8,16 @@ The CPU usage tracker exclusively operates within the Linux environment.
 #include "unistd.h"
 #include "stdbool.h"
 #include "pthread.h"
-#include "threads.h" //This library contains all threads used in this program
-
-struct CPU_s{   //'Class-like' structure used for storing CPU info and status
-    int total_usage;
-    int core_count;
-};
+#include "threads.h"
 
 void main(){
-    /* Initializing threads, variables etc. */
+    /* Initializing threads */
     pthread_t reader;
     pthread_create(&reader, NULL, reader_f, NULL);
 
+
     /* aaa */
     while(1){
-        printf("CPU usage tracker\n");
         sleep(10);
     };
-    sleep(1);
-    return NULL;
 }
--- threads.c
+++ threads.c
@@ -1,20 +1,49 @@
+/* This library contains the thread's functions
+    and their synchronization instructions. */
+
 #include "stdio.h"
+#include "stdlib.h"
 #include "stdbool.h"
-#include <unistd.h>
+#include "unistd.h"
+#include "pthread.h"
+
+/* Mutex and variables used for thread synchronization */
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int currentThread = 0; //0-reader ; 1-analizer ; 2-printer
+bool close_threads = false;
+char *raw_data;
+
+struct CPU_s{ //printer thread data
+    int total_usage;
+    int core_count;
+};
 
 void *reader_f(void *arg){
-    bool thread_exit = false;
-    char buffer[256];  // Buffer to hold the read line
-    while(1){
-        printf("reader:\n");
-        FILE *file = fopen("/stat", "r"); //change to /proc/stat
-        while (fgets(buffer, sizeof(buffer), file) != NULL) {
-            printf("\n %s", buffer);
-            sleep(1);
-        };
+    /*
+    This function reads a file that contains CPU usage information
+    and passes it to a global variable. 
+    */
+    int threadId = 0;
+    while(!close_threads){
+        pthread_mutex_lock(&mutex);
+        FILE *file = fopen("stat", "r"); //change to /proc/stat
+        fseek(file, 0, SEEK_END); //determine length of buffer char array
+        long fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        char *buffer = (char*)malloc(fileSize * sizeof(char));
+        fread(buffer, sizeof(char), fileSize, file); //reding stat file to buffer
         fclose(file);
-        sleep(3);
+        raw_data = buffer; //Sending current read to a global variable
+        free(buffer);
+
+        currentThread++;
+        pthread_mutex_unlock(&mutex);
+        
+        /* Check if it's reader's turn */
+        while (currentThread != threadId) { 
+            pthread_mutex_unlock(&mutex);
+            pthread_mutex_lock(&mutex);
+        };
     };
-    printf("end of thread\n");
-    return NULL;
+    pthread_exit(NULL);
 }
